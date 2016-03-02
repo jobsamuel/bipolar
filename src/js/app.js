873e1bcc50adc6283f5467e1b76d9f561602a2e2
Jobsamuel Núñez <jobsamuel.nunez@outlook.com>
2016-03-02 08:04:49 -0430

    Agrega nuevos compotentes

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,39 @@
-const Header = React.createClass({
+const Title = React.createClass({
   render: function() {
     return (
-      <div className="verb">
+      <div className="text-center">
         <h1>{this.props.data}</h1>
       </div>
     )
   }
 });
 
+const Buttons = React.createClass({
+  render: function() {
+    return (
+      <div className="button-group expanded">
+        <a className="button alert">Negativo</a>
+        <a className="button">Neutral</a>
+        <a className="button success">Positivo</a>
+      </div>
+    )
+  }
+});
+
+const Container = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <div className="small-10 medium-6 large-4 small-centered medium-centered large-centered">
+          <Title data="Bipolar" />
+          <Buttons />
+        </div>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
-  <Header data="Bipolar" />,
+  <Container />,
   document.getElementById('content')
 );
